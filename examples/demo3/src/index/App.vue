fcf1591a536f6dae5a71b5bd780f2e2ca24d46d8
juneandgreen <june_01@qq.com>
2019-08-07 12:42:37 +0800

    update: demo3 add getContext example

--- examples/demo3/src/index/App.vue
+++ examples/demo3/src/index/App.vue
@@ -114,26 +114,25 @@ export default {
     }
   },
   mounted() {
-    // 暂时有问题，需要基础库修复
-    // const canvas = this.$refs.canvas[0]
-    // canvas.$$getContext().then(context => {
-    //   context.setStrokeStyle("#00ff00")
-    //   context.setLineWidth(5)
-    //   context.rect(0, 0, 200, 200)
-    //   context.stroke()
-    //   context.setStrokeStyle("#ff0000")
-    //   context.setLineWidth(2)
-    //   context.moveTo(160, 100)
-    //   context.arc(100, 100, 60, 0, 2 * Math.PI, true)
-    //   context.moveTo(140, 100)
-    //   context.arc(100, 100, 40, 0, Math.PI, false)
-    //   context.moveTo(85, 80)
-    //   context.arc(80, 80, 5, 0, 2 * Math.PI, true)
-    //   context.moveTo(125, 80)
-    //   context.arc(120, 80, 5, 0, 2 * Math.PI, true)
-    //   context.stroke()
-    //   context.draw()
-    // })
+    const canvas = this.$refs.canvas[0]
+    canvas.$$getContext().then(context => {
+      context.setStrokeStyle("#00ff00")
+      context.setLineWidth(5)
+      context.rect(0, 0, 200, 200)
+      context.stroke()
+      context.setStrokeStyle("#ff0000")
+      context.setLineWidth(2)
+      context.moveTo(160, 100)
+      context.arc(100, 100, 60, 0, 2 * Math.PI, true)
+      context.moveTo(140, 100)
+      context.arc(100, 100, 40, 0, Math.PI, false)
+      context.moveTo(85, 80)
+      context.arc(80, 80, 5, 0, 2 * Math.PI, true)
+      context.moveTo(125, 80)
+      context.arc(120, 80, 5, 0, 2 * Math.PI, true)
+      context.stroke()
+      context.draw()
+    })
   },
   methods: {
     onInput(evt) {
