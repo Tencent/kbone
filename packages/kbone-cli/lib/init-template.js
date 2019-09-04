1684a498e097bcc02b31b077009e891ecb46613d
dntzhang <dntzhang@qq.com>
2019-09-04 10:59:53 +0800

    eslint

--- packages/kbone-cli/lib/init-template.js
+++ packages/kbone-cli/lib/init-template.js
@@ -1,122 +1,127 @@
-var path = require('path');
-var join = path.join;
-var basename = path.basename;
-var fs = require('fs');
-var existsSync = fs.existsSync;
-var chalk = require('chalk');
-var emptyDir = require('empty-dir');
-var info = require('./logger').info;
-var error = require('./logger').error;
-var success = require('./logger').success;
-var isCnFun = require('./utils').isCnFuc;
-var checkAppName = require('./utils').checkAppName;
-var isSafeToCreateProjectIn = require('./utils').isSafeToCreateProjectIn;
-var spawn = require('cross-spawn');
+const path = require('path')
+
+const join = path.join
+const basename = path.basename
+const fs = require('fs')
+const spawn = require('cross-spawn')
+
+const existsSync = fs.existsSync
+const chalk = require('chalk')
+const emptyDir = require('empty-dir')
+const info = require('./logger').info
+const error = require('./logger').error
+const success = require('./logger').success
+const isCnFun = require('./utils').isCnFuc
+const checkAppName = require('./utils').checkAppName
+const isSafeToCreateProjectIn = require('./utils').isSafeToCreateProjectIn
+
 
 function init(args) {
-  var KboneCli = chalk.bold.cyan("Kbone CLI");
-  var isCn = isCnFun(args.language);
-  var customPrjName = args.project || '';
-  var templateName = args.template || '';
-  var dest = join(process.cwd(), customPrjName);
-  var projectName = basename(dest);
-  var mirror = args.mirror;
+    const KboneCli = chalk.bold.cyan('Kbone CLI')
+    let isCn = isCnFun(args.language)
+    const customPrjName = args.project || ''
+    const templateName = args.template || ''
+    const dest = join(process.cwd(), customPrjName)
+    const projectName = basename(dest)
+    const mirror = args.mirror
 
-  isCn = true
-  console.log(KboneCli + (!isCn ? ' is booting... ' : ' 正在启动...'));
-  console.log(
-    KboneCli +
-    (!isCn ? ' will execute init command... ' : ' 即将执行 init 命令...')
-  );
-  checkAppName(projectName);
-  if (existsSync(dest) && !emptyDir.sync(dest)) {
-    if (!isSafeToCreateProjectIn(dest, projectName)) {
-      process.exit(1);
-    }
-  }
-
-  createApp();
-
-  function createApp() {
+    isCn = true
+    console.log(KboneCli + (!isCn ? ' is booting... ' : ' 正在启动...'))
     console.log(
-      chalk.bold.cyan('Kbone CLI') +
-      (!isCn
-        ? ' will creating a new kbone app in '
-        : ' 即将创建一个新的应用在 ') +
-      dest
-    );
-
-    const { status, error: cloneError } = spawn.sync('git', ['clone', '--depth=1', templateName !== 'omi' ? `https://github.com/wechat-miniprogram/kbone-template-${templateName}` : `https://github.com/omijs/template-kbone`, customPrjName || '.']);
-
-    // verify git clone succeed
-    if (!cloneError && status === 0) {
-      try {
-        try {
-          // remove .git
-          const gitPath = join(dest, '.git');
-          if (existsSync(gitPath)) {
-            spawn.sync('rm', ['-rf', gitPath])
-          }
-          // change a project name if project.config.json exist
-          if (existsSync(join(dest, 'project.config.json'))) {
-            var appProjectJson = require(join(dest, 'project.config.json'));
-            appProjectJson.projectname = projectName;
-            fs.writeFile(join(dest, 'project.config.json'), JSON.stringify(appProjectJson, null, 2), (err) => {
-              if (err) return console.log(err);
-            });
-          }
-          // change a package name as a project name if package.json exist
-          if (existsSync(join(dest, 'package.json'))) {
-            var appPackage = require(join(dest, 'package.json'));
-            appPackage.name = projectName;
-            fs.writeFile(join(dest, 'package.json'), JSON.stringify(appPackage, null, 2), (err) => {
-              if (err) return console.log(err);
-            });
-            process.chdir(customPrjName || '.');
-
-            // install ndoe package modules
-            info(
-              '正在安装依赖',
-              '你可以使用 ctrl + c 退出，然后自行安装。'
-            );
-            console.log();
-            require('./install')(mirror, done); // npm install
-          } else {
-            done();
-          }
-        } catch (e) {
-          console.log(error(e));
+        KboneCli +
+    (!isCn ? ' will execute init command... ' : ' 即将执行 init 命令...')
+    )
+    checkAppName(projectName)
+    if (existsSync(dest) && !emptyDir.sync(dest)) {
+        if (!isSafeToCreateProjectIn(dest, projectName)) {
+            process.exit(1)
         }
-      } catch (e) {
-        console.log(error(e));
-      }
-    } else {
-      // if incorrect template name
-      error(`模板 ${templateName} 不存在`);
     }
-  }
 
-  function done() {
-    console.log();
-    console.log();
-    console.log();
-    
-    console.log(`跳转目录`);
-    success(`cd ${projectName}`);
-    console.log();
-    console.log(`开发小程序`);
-    success(`npm start`);
-    console.log();
-    console.log(`开发 Web`);
-    success(`npm run web`);
-    console.log(`发布 Web`);
-    success(`npm run build`);
-    console.log();
-    console.log();
-    success(`恭喜你! "${projectName}"项目初始化成功! `);
-    console.log();
-    console.log();
-  }
+    createApp()
+
+    function createApp() {
+        console.log(
+            chalk.bold.cyan('Kbone CLI') +
+      (!isCn
+          ? ' will creating a new kbone app in '
+          : ' 即将创建一个新的应用在 ') +
+      dest
+        )
+
+        const {status, error: cloneError} = spawn.sync('git', ['clone', '--depth=1', templateName !== 'omi' ? `https://github.com/wechat-miniprogram/kbone-template-${templateName}` : 'https://github.com/omijs/template-kbone', customPrjName || '.'])
+
+        // verify git clone succeed
+        if (!cloneError && status === 0) {
+            try {
+                try {
+                    // remove .git
+                    const gitPath = join(dest, '.git')
+                    if (existsSync(gitPath)) {
+                        spawn.sync('rm', ['-rf', gitPath])
+                    }
+                    // change a project name if project.config.json exist
+                    if (existsSync(join(dest, 'project.config.json'))) {
+                        // eslint-disable-next-line
+                        const appProjectJson = require(join(dest, 'project.config.json'))
+                        appProjectJson.projectname = projectName
+                        fs.writeFile(join(dest, 'project.config.json'), JSON.stringify(appProjectJson, null, 2), (err) => {
+                            if (err) return console.log(err)
+                        })
+                    }
+                    // change a package name as a project name if package.json exist
+                    if (existsSync(join(dest, 'package.json'))) {
+                        // eslint-disable-next-line
+                        const appPackage = require(join(dest, 'package.json'))
+                        appPackage.name = projectName
+                        fs.writeFile(join(dest, 'package.json'), JSON.stringify(appPackage, null, 2), (err) => {
+                            if (err) return console.log(err)
+                        })
+                        process.chdir(customPrjName || '.')
+
+                        // install ndoe package modules
+                        info(
+                            '正在安装依赖',
+                            '你可以使用 ctrl + c 退出，然后自行安装。'
+                        )
+                        console.log()
+                        require('./install')(mirror, done) // npm install
+                    } else {
+                        done()
+                    }
+                } catch (e) {
+                    console.log(error(e))
+                }
+            } catch (e) {
+                console.log(error(e))
+            }
+        } else {
+            // if incorrect template name
+            error(`模板 ${templateName} 不存在`)
+        }
+    }
+
+    function done() {
+        console.log()
+        console.log()
+        console.log()
+
+        console.log('跳转目录')
+        success(`cd ${projectName}`)
+        console.log()
+        console.log('开发小程序')
+        success('npm start')
+        console.log()
+        console.log('开发 Web')
+        success('npm run web')
+        console.log('发布 Web')
+        success('npm run build')
+        console.log()
+        console.log()
+        success(`恭喜你! "${projectName}"项目初始化成功! `)
+        console.log()
+        console.log()
+    }
 }
 
-module.exports = init;
+module.exports = init
--- packages/kbone-cli/lib/install.js
+++ packages/kbone-cli/lib/install.js
@@ -1,37 +1,36 @@
-var spawn = require("cross-spawn");
-var ora = require("ora");
-var which = require("which");
+const spawn = require('cross-spawn')
+const which = require('which')
 
-module.exports = function (mirror, done) {
-	var registry,
-		resolved,
-		pkgtool = "npm";
-	mirror = mirror.toLowerCase();
-	if (mirror === "yarn" || mirror === "cnpm") {
-		try {
-			resolved = which.sync(mirror);
-			if (resolved) pkgtool = mirror;
-		} catch (e) {
-			console.log(e);
-			return secede(spawn(pkgtool, ["install"], { stdio: "inherit" }), done);
-		}
-	} else if (mirror !== "default") {
-		registry = ["--registry", require("./mirror")[mirror]];
-	}
-	return secede(
-		spawn(pkgtool, registry ? [].concat(["install"], registry) : ["install"], {
-			stdio: "inherit"
-		}), done
-	);
-};
+module.exports = function(mirror, done) {
+    let registry
+    let resolved
+    let pkgtool = 'npm'
+    mirror = mirror.toLowerCase()
+    if (mirror === 'yarn' || mirror === 'cnpm') {
+        try {
+            resolved = which.sync(mirror)
+            if (resolved) pkgtool = mirror
+        } catch (e) {
+            console.log(e)
+            return secede(spawn(pkgtool, ['install'], {stdio: 'inherit'}), done)
+        }
+    } else if (mirror !== 'default') {
+        registry = ['--registry', require('./mirror')[mirror]]
+    }
+    return secede(
+        spawn(pkgtool, registry ? [].concat(['install'], registry) : ['install'], {
+            stdio: 'inherit'
+        }), done
+    )
+}
 
 function secede(line, done) {
-	line.on("close", function (code) {
-		done && typeof done === 'function' && done();
-		process.exit(code);
-	});
+    line.on('close', function(code) {
+        if (done && typeof done === 'function') done()
+        process.exit(code)
+    })
 
-	line.on("error", function (reason) {
-		console.log("An error occured while executing the NPM command.", reason);
-	});
+    line.on('error', function(reason) {
+        console.log('An error occured while executing the NPM command.', reason)
+    })
 }
