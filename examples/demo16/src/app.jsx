20c8159912722b78ce36c49961e0f6679e2ed1b7
juneandgreen <june_01@qq.com>
2020-01-16 15:02:52 +0800

    fixed: fixed float number equals bug

--- examples/demo16/src/app.jsx
+++ examples/demo16/src/app.jsx
@@ -13,8 +13,8 @@ const App = (props, store) => {
                     onFocus={e => console.log('focus', e)}
                     onBlur={e => console.log('blur', e)}
                     onChange={e => console.log('change', e)}
-                    onConfirm={e => console.log('confirm', e)}
-                    onKeyBoardHeightChange={e => console.log('KeyBoardHeightChange', e)}
+                    onconfirm={e => console.log('confirm', e)}
+                    onkeyboardheightchange={e => console.log('KeyBoardHeightChange', e)}
                 />
             </div>
             <div>
