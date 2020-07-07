cba94f583d0b7527a792a58042163f5d30557e6e
juneandgreen <june_01@qq.com>
2020-07-07 18:12:36 +0800

    feature: add xhr protect after page unloaded

--- packages/miniprogram-element/src/base.js
+++ packages/miniprogram-element/src/base.js
@@ -106,7 +106,7 @@ module.exports = Behavior({
                 else console.warn(`value "${data.wxCompName}" is not supported for wx-component's behavior`)
             }
         },
-        
+
         /**
          * 监听子节点变化
          */
@@ -145,10 +145,10 @@ module.exports = Behavior({
             const domNode = this.domNode
             const data = this.data
             const tagName = domNode.tagName
-            
+
             // 使用 template 渲染
             if (USE_TEMPLATE.indexOf(tagName) !== -1 || USE_TEMPLATE.indexOf(domNode.behavior) !== -1) return
-            
+
             if (tagName === 'WX-COMPONENT') {
                 // 内置组件，目前只有 view、cover-view 和 scroll-view 组件需要进入
                 const newData = {}
