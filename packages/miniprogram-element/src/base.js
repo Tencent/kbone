a2b13258b99b75552eecec50a0694c4a3756a905
juneandgreen <june_01@qq.com>
2020-07-10 14:21:39 +0800

    update: warning for 2.x

--- packages/miniprogram-element/src/base.js
+++ packages/miniprogram-element/src/base.js
@@ -20,7 +20,7 @@ const {
 const MAX_DOM_SUB_TREE_LEVEL = 10
 let DOM_SUB_TREE_LEVEL = 10
 
-console.warn('当前渲染模式版本：miniprogram-element@2.x 版本（要求最低基础库版本 2.11.2）。\n\n2.x 版本对比 1.x 版本去除了渲染内置组件时额外引入的一层节点，如果升级版本过程中遇到样式错乱问题，可尝试去除使用 1.x 版本时额外追加的兼容样式，也可选择退回 1.x 版本（退回版本可以使用 generate.renderVersion 和 generate.elementVersion 配置：https://wechat-miniprogram.github.io/kbone/docs/config/#generate-renderversion ）。')
+console.warn('当前渲染模式版本：miniprogram-element@2.x 版本（建议最低基础库版本 2.11.2）。\n\n2.x 版本对比 1.x 版本去除了渲染内置组件时额外引入的一层节点，如果升级版本过程中遇到样式错乱问题，可尝试去除使用 1.x 版本时额外追加的兼容样式，也可选择退回 1.x 版本（退回版本可以使用 generate.renderVersion 和 generate.elementVersion 配置：https://wechat-miniprogram.github.io/kbone/docs/config/#generate-renderversion ）。')
 
 const version = wx.getSystemInfoSync().SDKVersion
 const behaviors = []
