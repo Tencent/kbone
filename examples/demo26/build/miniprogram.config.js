92e790eb87fc99256188794756e98bbf8e25d2f7
juneandgreen <june_01@qq.com>
2021-05-28 15:21:37 +0800

    update: demo26

--- examples/demo26/build/miniprogram.config.js
+++ examples/demo26/build/miniprogram.config.js
@@ -10,6 +10,9 @@ module.exports = {
 		notFound: 'page1',		
 		accessDenied: 'page1',
 	},
+	generate: {
+		globalVars: [['SVGElement', 'function SVGElement() {}']], // 兼容 vue3 3.0.8+ 版本
+	},
 	app: {
 		navigationBarTitleText: 'miniprogram-project',
 	},
