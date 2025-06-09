c4ce950dd2ce053998d832d8dee9444332407b14
junexie <junexie@tencent.com>
2025-06-09 15:28:40 +0800

    chore: update demo

--- examples/demo26/src/page1/App.vue
+++ examples/demo26/src/page1/App.vue
@@ -6,76 +6,72 @@
     <button @click="onClickJump">当前页跳转</button>
     <button @click="onClickOpen">新开页面跳转</button>
     <canvas ref="canvasRef" type="2d" width="300" height="200" @touchstart="log('normal', $event)" @canvastouchstart="log('canvas', $event)"></canvas>
+    <input v-model="inputValue" @input="onInput"/>
     <Footer></Footer>
   </div>
 </template>
 
-<script>
-import * as Vue from 'vue'
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 import Header from '../common/Header.vue'
 import Footer from '../common/Footer.vue'
 
-export default {
-  components: {
-    Header,
-    Footer
-  },
-  setup() {
-    const canvasRef = Vue.ref(null)
+const canvasRef = ref(null)
+const inputValue = defineModel({ type: String, default: 'test' })
+console.log('@@@@@@@@@@@@@@', inputValue)
 
-    Vue.onMounted(() => {
-      console.log('page1 mounted')
+onMounted(() => {
+  console.log('page1 mounted')
 
-      // 不要问为什么不用 $$prepare，vue3 把所有挂在 dom 上的东西都加了一层 proxy，导致基于 this 的 weakmap 全都没法用
-      const canvas = canvasRef.value
-      canvas.$$getNodesRef().then(nodesRef => nodesRef.node(res => {
-        const {width, height} = canvas
-        const node = res.node
+  // 不要问为什么不用 $$prepare，vue3 把所有挂在 dom 上的东西都加了一层 proxy，导致基于 this 的 weakmap 全都没法用
+  const canvas = canvasRef.value
+  canvas.$$getNodesRef().then(nodesRef => nodesRef.node(res => {
+    const {width, height} = canvas
+    const node = res.node
 
-        // 设置 canvas 宽高
-        node.width = width
-        node.height = height
+    // 设置 canvas 宽高
+    node.width = width
+    node.height = height
 
-        const context = node.getContext('2d')
+    const context = node.getContext('2d')
 
-        context.strokeStyle = '#00ff00'
-        context.lineWidth = 5
-        context.rect(0, 0, 200, 200)
-        context.stroke()
-        context.strokeStyle = '#ff0000'
-        context.lineWidth = 2
-        context.moveTo(160, 100)
-        context.arc(100, 100, 60, 0, 2 * Math.PI, true)
-        context.moveTo(140, 100)
-        context.arc(100, 100, 40, 0, Math.PI, false)
-        context.moveTo(85, 80)
-        context.arc(80, 80, 5, 0, 2 * Math.PI, true)
-        context.moveTo(125, 80)
-        context.arc(120, 80, 5, 0, 2 * Math.PI, true)
-        context.stroke()
-      }).exec()).catch(console.error)
-    })
+    context.strokeStyle = '#00ff00'
+    context.lineWidth = 5
+    context.rect(0, 0, 200, 200)
+    context.stroke()
+    context.strokeStyle = '#ff0000'
+    context.lineWidth = 2
+    context.moveTo(160, 100)
+    context.arc(100, 100, 60, 0, 2 * Math.PI, true)
+    context.moveTo(140, 100)
+    context.arc(100, 100, 40, 0, Math.PI, false)
+    context.moveTo(85, 80)
+    context.arc(80, 80, 5, 0, 2 * Math.PI, true)
+    context.moveTo(125, 80)
+    context.arc(120, 80, 5, 0, 2 * Math.PI, true)
+    context.stroke()
+  }).exec()).catch(console.error)
+})
 
-    Vue.onUnmounted(() => {
-      console.log('page1 unmounted')
-    })
+onUnmounted(() => {
+  console.log('page1 unmounted')
+})
 
-    return {
-      canvasRef,
+function onClickJump() {
+  window.location.href = '/b'
+}
 
-      onClickJump() {
-        window.location.href = '/b'
-      },
+function onClickOpen() {
+  window.open('/c')
+}
 
-      onClickOpen() {
-        window.open('/c')
-      },
+function onInput(evt) {
+  console.log('input', evt, evt.target.value)
+  inputValue.value = evt.target.value
+}
 
-      async log(...args) {
-        console.log(...args)
-      },
-    }
-  },
+async function log(...args) {
+  console.log(...args)
 }
 </script>
 
